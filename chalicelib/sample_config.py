9c224a7a7bf04625d8a0bf49612d94134db4dd8d
lovetrading10 <lovetrading09@yahoo.com>
2021-05-05 17:50:01 -0600

    Initial commit

---
+++ chalicelib/sample_config.py
@@ -0,0 +1,8 @@
+SLACK_API_TOKEN = '<your slack token>'
+SLACK_VERIFICATION_TOKEN = '<your slack verification token>'
+IEX_TOKEN = '<your iex token>'
+
+QUOTE_URL = "https://cloud.iexapis.com/stable/stock/{}/quote?token={}"
+CHART_URL = "https://finviz.com/chart.ashx?t={}"
+
+SCHEDULED_SYMBOLS = ['DIA', 'SPY', 'QQQ', 'IWM']
